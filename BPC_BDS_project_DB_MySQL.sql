b2641ef76b2f2f2b76063e3abfe8ca0296b10a16
AdamKubis <58268918+AdamKubis@users.noreply.github.com>
2021-10-28 22:41:44 +0200

    Update BPC_BDS_project_DB_MySQL.sql

--- BPC_BDS_project_DB_MySQL.sql
+++ BPC_BDS_project_DB_MySQL.sql
@@ -1,7 +1,13 @@
 -- -----------------------------------------------------
+-- 	   1. Project Assignment - Database Design
+-- 				  BPC_BDS_MySQL
+-- Authors: Adam Kubiš(213431), Jakub Jarina(230086)
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
 -- Schema BPC_BDS_Project
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `BPC_BDS_Project`;
+CREATE SCHEMA IF NOT EXISTS `BPC_BDS_Project` DEFAULT CHARACTER SET utf8;
 USE `BPC_BDS_Project`;
 
 -- -----------------------------------------------------
@@ -11,13 +17,12 @@ CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`users` (
   `id_user` INT NOT NULL,
   `first_name` VARCHAR(50) NOT NULL,
   `last_name` VARCHAR(50) NOT NULL,
-  `registration` TIMESTAMP NOT NULL,
-  `last_login` TIMESTAMP NOT NULL,
+  `registration` DATETIME NOT NULL,
+  `last_login` DATETIME NOT NULL,
   `email` VARCHAR(50) NOT NULL,
   `phone_number` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`id_user`),
-  UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC) VISIBLE);
-
+  PRIMARY KEY (`id_user`))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `BPC_BDS_Project`.`address`
@@ -30,198 +35,205 @@ CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`address` (
   `city` VARCHAR(50) NOT NULL,
   `country` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id_address`),
-  UNIQUE INDEX `id_address_UNIQUE` (`id_address` ASC) VISIBLE)
+  INDEX `id_address_UNIQUE` (`id_address` ASC))
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`order_status`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`order_status` (
+  `status_code` INT NOT NULL,
+  `status_description` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`status_code`))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`user_has_address`
+-- Table `BPC_BDS_Project`.`payment_type`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`user_has_address` (
-  `id_user` INT NOT NULL,
-  `id_address` INT NOT NULL,
-  PRIMARY KEY (`id_user`, `id_address`),
-  INDEX `fk_user_has_address_idx` (`id_user` ASC) INVISIBLE,
-  INDEX `fk_user_has_address2_idx` (`id_address` ASC) VISIBLE,
-  UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC) VISIBLE,
-  UNIQUE INDEX `id_address_UNIQUE` (`id_address` ASC) VISIBLE,
-  CONSTRAINT `fk_user_has_address_user`
-    FOREIGN KEY (`id_user`)
-    REFERENCES `BPC_BDS_Project`.`users` (`id_user`)
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`payment_type` (
+  `payment_type_id` INT NOT NULL,
+  `payment_method` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`payment_type_id`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`payment_status`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`payment_status` (
+  `id_payment_status` INT NOT NULL,
+  `payment_status_type` VARCHAR(45) NOT NULL,
+  `paid_at` DATETIME NULL,
+  PRIMARY KEY (`id_payment_status`))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`payment`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`payment` (
+  `id_payment` INT NOT NULL,
+  `payment_type` INT NOT NULL,
+  `id_payment_status` INT NOT NULL,
+  `subtotal` REAL NOT NULL,
+  `discount` REAL NOT NULL,
+  `shippment` REAL NOT NULL,
+  `total` REAL NOT NULL,
+  PRIMARY KEY (`id_payment`, `payment_type`, `id_payment_status`),
+  INDEX `fk_payment_has_type_idx` (`payment_type` ASC),
+  INDEX `fk_payment_payment_status1_idx` (`id_payment_status` ASC),
+  CONSTRAINT `fk_payment_has_type`
+    FOREIGN KEY (`payment_type`)
+    REFERENCES `BPC_BDS_Project`.`payment_type` (`payment_type_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_address_address`
-    FOREIGN KEY (`id_address`)
-    REFERENCES `BPC_BDS_Project`.`address` (`id_address`)
+  CONSTRAINT `fk_payment_payment_status1`
+    FOREIGN KEY (`id_payment_status`)
+    REFERENCES `BPC_BDS_Project`.`payment_status` (`id_payment_status`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`product`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`product` (
-  `id_product` INT NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_product`),
-  UNIQUE INDEX `id_product_UNIQUE` (`id_product` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`brand`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`brand` (
-  `id_brand` INT NOT NULL,
-  `brand` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_brand`),
-  UNIQUE INDEX `brand_UNIQUE` (`brand` ASC) VISIBLE,
-  UNIQUE INDEX `id_brand_UNIQUE` (`id_brand` ASC) VISIBLE)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`product_has_brand`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`product_has_brand` (
-  `id_product` INT NOT NULL,
-  `id_brand` INT NOT NULL,
-  PRIMARY KEY (`id_product`, `id_brand`),
-  INDEX `fk_product_has_brand1_idx` (`id_brand` ASC) INVISIBLE,
-  UNIQUE INDEX `id_product_UNIQUE` (`id_product` ASC) VISIBLE,
-  UNIQUE INDEX `id_brand_UNIQUE` (`id_brand` ASC) VISIBLE,
-  INDEX `fk_product_has_brand2_idx` (`id_product` ASC) VISIBLE,
-  CONSTRAINT `fk_product_has_brand_product`
-    FOREIGN KEY (`id_product`)
-    REFERENCES `BPC_BDS_Project`.`product` (`id_product`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_product_has_brand_brand`
-    FOREIGN KEY (`id_brand`)
-    REFERENCES `BPC_BDS_Project`.`brand` (`id_brand`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `BPC_BDS_Project`.`order`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`order` (
   `id_order` INT NOT NULL,
-  `subtotal` FLOAT NOT NULL,
-  `discount` FLOAT NOT NULL,
-  `shipping` FLOAT NOT NULL,
-  `total` FLOAT NOT NULL,
-  PRIMARY KEY (`id_order`),
-  UNIQUE INDEX `id_order_UNIQUE` (`id_order` ASC) VISIBLE)
+  `order_status_code` INT NOT NULL,
+  `id_payment` INT NOT NULL,
+  PRIMARY KEY (`id_order`, `order_status_code`, `id_payment`),
+  INDEX `fk_order_order_status1_idx` (`order_status_code` ASC),
+  INDEX `fk_order_payment1_idx` (`id_payment` ASC),
+  CONSTRAINT `fk_order_order_status1`
+    FOREIGN KEY (`order_status_code`)
+    REFERENCES `BPC_BDS_Project`.`order_status` (`status_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_payment1`
+    FOREIGN KEY (`id_payment`)
+    REFERENCES `BPC_BDS_Project`.`payment` (`id_payment`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`product_type`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`product_type` (
+  `product_type_id` INT NOT NULL,
+  `product_type_description` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`product_type_id`))
+ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`user_has_order`
+-- Table `BPC_BDS_Project`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`user_has_order` (
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`product` (
+  `id_product` INT NOT NULL,
+  `id_product_type` INT NOT NULL,
+  `product_brand_name` VARCHAR(45) NOT NULL,
+  `product_model` VARCHAR(45) NOT NULL,
+  `product_version` VARCHAR(45) NULL,
+  `product_color` VARCHAR(45) NULL,
+  `product_price` REAL NOT NULL,
+  `in_stock` INT NOT NULL,
+  PRIMARY KEY (`id_product`, `id_product_type`),
+  INDEX `fk_product_has_type_idx` (`id_product_type` ASC),
+  CONSTRAINT `fk_product_has_type`
+    FOREIGN KEY (`id_product_type`)
+    REFERENCES `BPC_BDS_Project`.`product_type` (`product_type_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`users_has_order`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`users_has_order` (
   `id_user` INT NOT NULL,
   `id_order` INT NOT NULL,
-  `status` VARCHAR(50) NOT NULL,
-  INDEX `fk_user_has_order1_idx` (`id_order` ASC) INVISIBLE,
-  UNIQUE INDEX `id_user_UNIQUE` (`id_user` ASC) VISIBLE,
-  UNIQUE INDEX `id_order_UNIQUE` (`id_order` ASC) VISIBLE,
   PRIMARY KEY (`id_user`, `id_order`),
-  INDEX `fk_user_has_order2_idx` (`id_user` ASC) VISIBLE,
-  CONSTRAINT `fk_user_has_order_user`
+  INDEX `fk_users_has_order_order1_idx` (`id_order` ASC),
+  INDEX `fk_users_has_order_users1_idx` (`id_user` ASC),
+  CONSTRAINT `fk_users_has_order_users1`
     FOREIGN KEY (`id_user`)
     REFERENCES `BPC_BDS_Project`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_has_order_order`
+  CONSTRAINT `fk_users_has_order_order1`
     FOREIGN KEY (`id_order`)
     REFERENCES `BPC_BDS_Project`.`order` (`id_order`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`transaction`
+-- Table `BPC_BDS_Project`.`order_has_product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`transaction` (
-  `id_translation` INT NOT NULL,
-  `id_order` INT NOT NULL,
-  `payment` VARCHAR(50) NOT NULL,
-  `paid_at` DATETIME NOT NULL,
-  PRIMARY KEY (`id_translation`, `id_order`),
-  INDEX `fk_transaction_order1_idx` (`id_order` ASC) VISIBLE,
-  UNIQUE INDEX `id_translation_UNIQUE` (`id_translation` ASC) VISIBLE,
-  UNIQUE INDEX `id_order_UNIQUE` (`id_order` ASC) VISIBLE,
-  CONSTRAINT `fk_transaction_order1`
-    FOREIGN KEY (`id_order`)
-    REFERENCES `BPC_BDS_Project`.`order` (`id_order`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`ordered_has_product`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`ordered_has_product` (
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`order_has_product` (
   `id_order` INT NOT NULL,
   `id_product` INT NOT NULL,
   `quantity` INT NOT NULL,
   PRIMARY KEY (`id_order`, `id_product`),
-  INDEX `fk_ordered_has_product1_idx` (`id_product` ASC) INVISIBLE,
-  UNIQUE INDEX `id_order_UNIQUE` (`id_order` ASC) VISIBLE,
-  UNIQUE INDEX `id_product_UNIQUE` (`id_product` ASC) VISIBLE,
-  INDEX `fk_ordered_has_product2_idx` (`id_order` ASC) VISIBLE,
-  CONSTRAINT `fk_ordered_has_product_order1`
+  INDEX `fk_order_has_product_product1_idx` (`id_product` ASC),
+  INDEX `fk_order_has_product_order1_idx` (`id_order` ASC),
+  CONSTRAINT `fk_order_has_product_order1`
     FOREIGN KEY (`id_order`)
     REFERENCES `BPC_BDS_Project`.`order` (`id_order`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ordered_has_product_product1`
+  CONSTRAINT `fk_order_has_product_product1`
     FOREIGN KEY (`id_product`)
     REFERENCES `BPC_BDS_Project`.`product` (`id_product`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`specification`
+-- Table `BPC_BDS_Project`.`account`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`specification` (
-  `id_specification` INT NOT NULL,
-  `product_type` VARCHAR(45) NOT NULL,
-  `model` VARCHAR(45) NULL,
-  `version` VARCHAR(45) NULL,
-  `in_stock` INT NOT NULL,
-  PRIMARY KEY (`id_specification`),
-  UNIQUE INDEX `id_specification_UNIQUE` (`id_specification` ASC) VISIBLE)
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`account` (
+  `id_account` INT NOT NULL,
+  `account_type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_account`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
--- Table `BPC_BDS_Project`.`product_has_specification`
+-- Table `BPC_BDS_Project`.`users_has_account`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`product_has_specification` (
-  `id_product` INT NOT NULL,
-  `id_specification` INT NOT NULL,
-  PRIMARY KEY (`id_product`, `id_specification`),
-  UNIQUE INDEX `id_product_UNIQUE` (`id_product` ASC) VISIBLE,
-  UNIQUE INDEX `id_specification_UNIQUE` (`id_specification` ASC) VISIBLE,
-  INDEX `fk_product_has_specification1_idx` (`id_product` ASC) VISIBLE,
-  INDEX `fk_product_has_specification2_idx` (`id_specification` ASC) VISIBLE,
-  CONSTRAINT `fk_product_has_specification_product1`
-    FOREIGN KEY (`id_product`)
-    REFERENCES `BPC_BDS_Project`.`product` (`id_product`)
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`users_has_account` (
+  `users_id_user` INT NOT NULL,
+  `account_id_account` INT NOT NULL,
+  PRIMARY KEY (`users_id_user`, `account_id_account`),
+  INDEX `fk_users_has_account_account1_idx` (`account_id_account` ASC),
+  INDEX `fk_users_has_account_users1_idx` (`users_id_user` ASC),
+  CONSTRAINT `fk_users_has_account_users1`
+    FOREIGN KEY (`users_id_user`)
+    REFERENCES `BPC_BDS_Project`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_product_has_specification_specification1`
-    FOREIGN KEY (`id_specification`)
-    REFERENCES `BPC_BDS_Project`.`specification` (`id_specification`)
+  CONSTRAINT `fk_users_has_account_account1`
+    FOREIGN KEY (`account_id_account`)
+    REFERENCES `BPC_BDS_Project`.`account` (`id_account`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `BPC_BDS_Project`.`users_has_address`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `BPC_BDS_Project`.`users_has_address` (
+  `id_user` INT NOT NULL,
+  `id_address` INT NOT NULL,
+  PRIMARY KEY (`id_user`, `id_address`),
+  INDEX `fk_users_has_address_address1_idx` (`id_address` ASC),
+  INDEX `fk_users_has_address_users1_idx` (`id_user` ASC),
+  CONSTRAINT `fk_users_has_address_users1`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `BPC_BDS_Project`.`users` (`id_user`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_address_address1`
+    FOREIGN KEY (`id_address`)
+    REFERENCES `BPC_BDS_Project`.`address` (`id_address`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
