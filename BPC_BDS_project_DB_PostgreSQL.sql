f7d3951b6f3441ca30ed3fefd6d5c4925ef669a9
AdamKubis <58268918+AdamKubis@users.noreply.github.com>
2021-10-29 03:29:00 +0200

    Update BPC_BDS_project_DB_PostgreSQL.sql

--- BPC_BDS_project_DB_PostgreSQL.sql
+++ BPC_BDS_project_DB_PostgreSQL.sql
@@ -3,19 +3,17 @@
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS bpc_bds_project
     AUTHORIZATION postgres;
-	
+
 -- -----------------------------------------------------
 -- Table BPC_BDS_Project.users
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS bpc_bds_project.users
 (
-    id_user INTEGER NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    registration TIMESTAMP NOT NULL,
-    last_login TIMESTAMP NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
+    id_user serial NOT NULL,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    email character varying(50) NOT NULL,
+    phone_number character varying(20) NOT NULL,
     PRIMARY KEY (id_user)
 );
 ALTER TABLE bpc_bds_project.users OWNER TO postgres;
@@ -25,185 +23,221 @@ ALTER TABLE bpc_bds_project.users OWNER TO postgres;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS bpc_bds_project.address
 (
-    id_address INTEGER NOT NULL,
-    street VARCHAR(50) NOT NULL,
-    house_number INTEGER NOT NULL,
-    postcode VARCHAR(45) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    country VARCHAR(50) NOT NULL,
+    id_address serial NOT NULL,
+    street character varying(50) NOT NULL,
+    house_number integer NOT NULL,
+    postcode character varying(45) NOT NULL,
+    city character varying(50) NOT NULL,
+    country character varying(50) NOT NULL,
     PRIMARY KEY (id_address)
 );
 ALTER TABLE bpc_bds_project.address OWNER TO postgres;
 
-
 -- -----------------------------------------------------
--- Table BPC_BDS_Project.user_has_address
+-- Table BPC_BDS_Project.account
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.user_has_address
+CREATE TABLE IF NOT EXISTS bpc_bds_project.account
 (
-    id_user INTEGER NOT NULL,
-    id_address INTEGER NOT NULL,
-	CONSTRAINT fk_user_has_address1
-		FOREIGN KEY (id_user)
-		REFERENCES bpc_bds_project.users (id_user)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION,
-  	CONSTRAINT fk_user_has_address2
-		FOREIGN KEY (id_address)
-		REFERENCES bpc_bds_project.address (id_address)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
+    id_account serial NOT NULL,
+    account_type character varying(45) NOT NULL,
+    PRIMARY KEY (id_account)
 );
-ALTER TABLE bpc_bds_project.user_has_address OWNER TO postgres;
-
+ALTER TABLE bpc_bds_project.account OWNER TO postgres;
 
 -- -----------------------------------------------------
--- Table BPC_BDS_Project.order
+-- Table BPC_BDS_Project.users_has_address
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.order
+CREATE TABLE IF NOT EXISTS bpc_bds_project.users_has_address
 (
-    id_order INTEGER NOT NULL,
-    subtotal REAL NOT NULL,
-    discount REAL NOT NULL,
-    shipping REAL NOT NULL,
-    total REAL NOT NULL,
-    PRIMARY KEY (id_order)
+    id_user serial NOT NULL,
+    id_address serial NOT NULL,
+		CONSTRAINT fk_user_has_address1
+			FOREIGN KEY (id_user)
+			REFERENCES bpc_bds_project.users (id_user)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  		CONSTRAINT fk_user_has_address2
+			FOREIGN KEY (id_address)
+			REFERENCES bpc_bds_project.address (id_address)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
 );
-ALTER TABLE bpc_bds_project.order OWNER TO postgres;
-
+ALTER TABLE bpc_bds_project.users_has_address OWNER TO postgres;
 
 -- -----------------------------------------------------
--- Table BPC_BDS_Project.user_has_order
+-- Table BPC_BDS_Project.users_has_account
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.user_has_order
+CREATE TABLE IF NOT EXISTS bpc_bds_project.users_has_account
 (
-    id_user INTEGER NOT NULL,
-    id_order INTEGER NOT NULL,
-    status VARCHAR(50) NOT NULL,
-	CONSTRAINT fk_user_has_order1
-		FOREIGN KEY (id_user)
-		REFERENCES bpc_bds_project.users (id_user)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION,
-	CONSTRAINT fk_user_has_order2
-		FOREIGN KEY (id_order)
-		REFERENCES bpc_bds_project.order (id_order)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
+    id_user serial NOT NULL,
+    id_account serial NOT NULL,
+    registration timestamp with time zone,
+    last_login timestamp with time zone,
+		CONSTRAINT fk_user_has_account1
+			FOREIGN KEY (id_user)
+			REFERENCES bpc_bds_project.users (id_user)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  		CONSTRAINT fk_user_has_account2
+			FOREIGN KEY (id_account)
+			REFERENCES bpc_bds_project.account (id_account)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
 );
-ALTER TABLE bpc_bds_project.user_has_order OWNER TO postgres;
-
+ALTER TABLE bpc_bds_project.users_has_account OWNER TO postgres;
 
 -- -----------------------------------------------------
--- Table BPC_BDS_Project.transaction
+-- Table BPC_BDS_Project.payment_type
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.transaction
+CREATE TABLE IF NOT EXISTS bpc_bds_project.payment_type
 (
-    id_transaction INTEGER,
-    id_order INTEGER,
-    payment VARCHAR(50),
-    paid_at TIMESTAMP,
-    PRIMARY KEY (id_transaction),
-	CONSTRAINT fk_transaction
-		FOREIGN KEY (id_order)
-		REFERENCES bpc_bds_project.order (id_order)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
+    payment_type serial NOT NULL,
+    payment_method character varying(45) NOT NULL,
+    PRIMARY KEY (payment_type)
 );
-ALTER TABLE bpc_bds_project.transaction OWNER TO postgres;
+ALTER TABLE bpc_bds_project.payment_type OWNER TO postgres;
+
+-- -----------------------------------------------------
+-- Table BPC_BDS_Project.payment_status
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bpc_bds_project.payment_status
+(
+    payment_status serial NOT NULL,
+    payment_status_type character varying(45) NOT NULL,
+    PRIMARY KEY (payment_status)
+);
+ALTER TABLE bpc_bds_project.payment_status OWNER TO postgres;
+
+-- -----------------------------------------------------
+-- Table BPC_BDS_Project.payment
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bpc_bds_project.payment
+(
+    id_payment serial NOT NULL,
+    payment_type serial NOT NULL,
+    id_payment_status serial NOT NULL,
+    subtotal real NOT NULL,
+    discount real NOT NULL,
+    shippment real NOT NULL,
+    total real NOT NULL,
+    paid_at timestamp with time zone,
+    PRIMARY KEY (id_payment),
+	CONSTRAINT fk_payment1
+			FOREIGN KEY (payment_type)
+			REFERENCES bpc_bds_project.payment_type (payment_type)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  	CONSTRAINT fk_payment2
+			FOREIGN KEY (id_payment_status)
+			REFERENCES bpc_bds_project.payment_status (payment_status)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+);
+ALTER TABLE bpc_bds_project.payment OWNER TO postgres;
+
+-- -----------------------------------------------------
+-- Table BPC_BDS_Project.order_status
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bpc_bds_project.order_status
+(
+    status_code serial NOT NULL,
+    status_description character varying(45) NOT NULL,
+    PRIMARY KEY (status_code)
+);
+ALTER TABLE bpc_bds_project.order_status OWNER TO postgres;
+
+-- -----------------------------------------------------
+-- Table BPC_BDS_Project.product_type
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bpc_bds_project.product_type
+(
+    product_type serial NOT NULL,
+    product_type_description character varying(45) NOT NULL,
+    PRIMARY KEY (product_type)
+);
+ALTER TABLE bpc_bds_project.product_type OWNER TO postgres;
 
 -- -----------------------------------------------------
 -- Table BPC_BDS_Project.product
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS bpc_bds_project.product
 (
-    id_product INTEGER NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id_product)
+    id_product serial NOT NULL,
+    id_product_type serial NOT NULL,
+    product_brand_name character varying(45) NOT NULL,
+    product_model character varying(45) NOT NULL,
+    product_version character varying(45),
+    product_color character varying(45),
+    product_price real NOT NULL,
+    in_stock integer NOT NULL,
+    PRIMARY KEY (id_product),
+	CONSTRAINT fk_product1
+			FOREIGN KEY (id_product_type)
+			REFERENCES bpc_bds_project.product_type (product_type)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
 );
 ALTER TABLE bpc_bds_project.product OWNER TO postgres;
+
+-- -----------------------------------------------------
+-- Table BPC_BDS_Project.order
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS bpc_bds_project.order
+(
+    id_order serial NOT NULL,
+    order_status_code serial NOT NULL,
+    id_payment serial NOT NULL,
+    PRIMARY KEY (id_order),
+	CONSTRAINT fk_order1
+			FOREIGN KEY (order_status_code)
+			REFERENCES bpc_bds_project.order_status (status_code)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  	CONSTRAINT fk_order2
+			FOREIGN KEY (id_payment)
+			REFERENCES bpc_bds_project.payment (id_payment)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
+);
+ALTER TABLE bpc_bds_project.order OWNER TO postgres;
 
 -- -----------------------------------------------------
 -- Table BPC_BDS_Project.order_has_product
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS bpc_bds_project.order_has_product
 (
-    id_order INTEGER NOT NULL,
-    id_product INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
+    id_order serial NOT NULL,
+    id_product serial NOT NULL,
+    quantity integer NOT NULL,
 	CONSTRAINT fk_order_has_product1
-		FOREIGN KEY (id_order)
-		REFERENCES bpc_bds_project.order (id_order)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION,
-	CONSTRAINT fk_order_has_product2
-		FOREIGN KEY (id_product)
-		REFERENCES bpc_bds_project.product (id_product)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
+			FOREIGN KEY (id_order)
+			REFERENCES bpc_bds_project.order (id_order)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  	CONSTRAINT fk_order_has_product2
+			FOREIGN KEY (id_product)
+			REFERENCES bpc_bds_project.product (id_product)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
 );
 ALTER TABLE bpc_bds_project.order_has_product OWNER TO postgres;
 
 -- -----------------------------------------------------
--- Table BPC_BDS_Project.brand
+-- Table BPC_BDS_Project.users_has_order
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.brand
+CREATE TABLE IF NOT EXISTS bpc_bds_project.users_has_order
 (
-    id_brand INTEGER NOT NULL,
-    brand VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id_brand)
+    id_user serial NOT NULL,
+    id_order serial NOT NULL,
+	CONSTRAINT fk_users_has_order1
+			FOREIGN KEY (id_user)
+			REFERENCES bpc_bds_project.users (id_user)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION,
+  	CONSTRAINT fk_users_has_order2
+			FOREIGN KEY (id_order)
+			REFERENCES bpc_bds_project.order (id_order)
+			ON DELETE NO ACTION
+			ON UPDATE NO ACTION
 );
-ALTER TABLE bpc_bds_project.brand OWNER TO postgres;
-
--- -----------------------------------------------------
--- Table BPC_BDS_Project.product_has_brand
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS bpc_bds_project.product_has_brand
-(
-    id_product INTEGER NOT NULL,
-    id_brand INTEGER NOT NULL,
-	CONSTRAINT fk_product_has_brand1
-		FOREIGN KEY (id_product)
-		REFERENCES bpc_bds_project.product (id_product)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION,
-	CONSTRAINT fk_product_has_brand2
-		FOREIGN KEY (id_brand)
-		REFERENCES bpc_bds_project.brand (id_brand)
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-);
-ALTER TABLE bpc_bds_project.product_has_brand OWNER TO postgres;
-
--- -----------------------------------------------------
--- Table BPC_BDS_Project.specification
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS BPC_BDS_Project.specification
-(
-  id_specification INTEGER NOT NULL,
-  product_type VARCHAR(45) NOT NULL,
-  model VARCHAR(45) NULL,
-  version VARCHAR(45) NULL,
-  in_stock INTEGER NOT NULL,
-  PRIMARY KEY (id_specification)
- );
- 
--- -----------------------------------------------------
--- Table BPC_BDS_Project.product_has_specification
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS BPC_BDS_Project.product_has_specification
-(
-  id_product INTEGER NOT NULL,
-  id_specification INTEGER NOT NULL,
-  PRIMARY KEY (id_product, id_specification),
-  CONSTRAINT fk_product_has_specification1
-    FOREIGN KEY (id_product)
-    REFERENCES BPC_BDS_Project.product (id_product)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT fk_product_has_specification2
-    FOREIGN KEY (id_specification)
-    REFERENCES BPC_BDS_Project.specification (id_specification)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
+ALTER TABLE bpc_bds_project.users_has_order OWNER TO postgres;
